be1d8fbc51d58c3f8ba03dd9742b8be473562fda
Adebayo Samuel <eladebayoor@gmail.com>
2021-12-23 16:16:57 +0100

    final edit before submission

--- designs.js
+++ designs.js
@@ -1,40 +1,63 @@
 // Select color input
-// Select size input
+
+let colorPicker = document.getElementById('colorPicker');
+let color = colorPicker.value;
+colorPicker.addEventListener('change', function(e){
+    color = e.target.value;
+});
+
 
 // When size is submitted by the user, call makeGrid()
+
+let table = document.getElementById('pixelCanvas');
 
 const form = document.getElementById('sizePicker');
 form.addEventListener('submit', makeGrid);
 
 
-function makeGrid(e) {
-    // Your code goes here!
-    e.preventDefault();
-    
-    const status = document.getElementById('status');
-    let table = document.getElementById('pixelCanvas');
 
+function makeGrid(event) {
+    event.preventDefault();
+
+    //reset grid to blank state
+    table.innerHTML = '';
+
+    // Select size input
     let innerWidth = document.getElementById('inputWidth').value;
     let innerHeight = document.getElementById('inputHeight').value;
 
-    //if table is empty
-    if (table.innerHTML === '') {
-        //create cells
-        for(let i = 0; i < innerWidth; i++) {
-            
-            let row = document.createElement("tr");
 
-            for (let j = 0; j < innerHeight; j++) {
-                let cell = document.createElement("td");
-                row.appendChild(cell);
-            }
+    //create cells
+    for(let i = 0; i < innerWidth; i++) {
 
-            table.appendChild(row);
+        let row = document.createElement("tr");
+
+        for (let j = 0; j < innerHeight; j++) {
+            let cell = document.createElement("td");
+            row.appendChild(cell);
         }
-        status.textContent = `Pixel Art Created! Time stamp: ${e.timeStamp}`
 
-    } else (table.innerHTML = '');
+        table.appendChild(row);
+    }
+
+    let cells = document.querySelectorAll("td");
+
+    //color grid pixels onClick
+    cells.forEach(function (cell) {
+        cell.addEventListener("click", function () {
+            cell.style.backgroundColor = color;
+        });
+    });
 
 }
+
+
+
+
+
+
+
+
+
 
 
